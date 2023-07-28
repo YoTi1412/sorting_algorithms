3cdb0cfb67b5ea3c1a486b39fa92ef14074e1193
YoTi <yasserrafai5@gmail.com>
2023-07-28 19:44:01 +0100

    task 100

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,41 +1,88 @@
 #include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-*swap - the positions of two elements into an array
-*@array: array
-*@item1: array element
-*@item2: array element
-*/
-void swap(int *array, int item1, int item2)
-{
-
-	int tmp;
-
-	tmp = array[item1];
-	array[item1] = array[item2];
-	array[item2] = tmp;
-}
-/**
- * shell_sort - function that sorts an array of integers in ascending
- * order using the Shell sort algorithm, using the Knuth sequence
- * @size: size of the array
- * @array: list with numbers
+ * _calloc - this is a calloc function
+ * @nmemb: number of elements
+ * @size: bit size of each element
+ * Return: pointer to memory assignment
  */
-void shell_sort(int *array, size_t size)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	size_t gap = 1, i, index = 0;
+    unsigned int i = 0;
+    void *p;
 
-	if (array == NULL || size < 2)
-		return;
-	while (gap < size / 3)
-		gap = 3 * gap + 1;
-	while (gap >= 1)
-	{
-		for (i = gap; i < size; i++)
-			for (index = i; index >= gap &&
-			 (array[index] < array[index - gap]); index -= gap)
-				swap(array, index, index - gap);
-		print_array(array, size);
-		gap /= 3;
-	}
+    if (nmemb == 0 || size == 0)
+        return NULL;
+
+    p = malloc(nmemb * size);
+    if (p == NULL)
+        return NULL;
+
+    char *char_ptr = (char *)p;
+    for (i = 0; i < (nmemb * size); i++)
+        char_ptr[i] = '\0';
+
+    return p;
 }
+
+/**
+ * counting_sort - this is a counting sort method implementation
+ * @array: array to sort
+ * @size: array size
+ */
+void counting_sort(int *array, size_t size)
+{
+    int index, maximun = 0, *counter = NULL, *tmp = NULL;
+    size_t i;
+
+    if (array == NULL || size < 2)
+        return;
+
+    /* Find maximum number */
+    for (i = 0; i < size; i++)
+    {
+        if (array[i] > maximun)
+            maximun = array[i];
+    }
+
+    counter = _calloc(maximun + 1, sizeof(int));
+    tmp = _calloc(size, sizeof(int));
+
+    if (counter == NULL || tmp == NULL)
+    {
+        free(counter);
+        free(tmp);
+        return;
+    }
+
+    /* Count the array elements */
+    for (i = 0; i < size; i++)
+    {
+        counter[array[i]]++;
+    }
+
+    /* Get the accumulative values */
+    for (index = 1; index <= maximun; index++)
+    {
+        counter[index] += counter[index - 1];
+    }
+
+    /* Get the new array sorted */
+    for (i = 0; i < size; i++)
+    {
+        tmp[counter[array[i]] - 1] = array[i];
+        counter[array[i]]--;
+    }
+
+    /* Replace old array with new array sorted */
+    for (i = 0; i < size; i++)
+    {
+        array[i] = tmp[i];
+    }
+
+    free(tmp);
+    free(counter);
+}
+
